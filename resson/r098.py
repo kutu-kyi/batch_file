3399cd91048586a915f7bba5ef3ebaefde360e57
kutu <kutsukake.kyi@gmail.com>
2021-06-25 13:12:11 +0900

    [test]

--- resson/r098.py
+++ resson/r098.py
@@ -1,9 +1,9 @@
 #テスト用(宮前平)
 
 shop_id = "SHP074"
-string = "パワーラッシュ45"
-instructor = "木本　優"
-s_date = "2021-06-15"
-e_date = "2021-06-15"
-time_s_reserve = "00:00:00"
+string = "エアロ１"
+instructor = "阿部　育美"
+#s_date = "2021-06-15"
+#e_date = "2021-06-15"
+time_s_reserve = "15:25:00"
 pos = "3:5"
