8332dae1d6f3276e1024831a5b0c3d9fdc447f71
kutu <kutsukake.kyi@gmail.com>
2021-07-05 09:33:03 +0900

    [追加]

---
+++ resson/r006.py
@@ -0,0 +1,9 @@
+#日曜日/三軒茶屋/有里巳さん/オリジナル
+
+shop_id = "SHP051"
+string = "エアロ１"
+instructor = "有里巳"
+#s_date = "2021-06-13"
+#e_date = "2021-06-13"
+time_s_reserve = "17:05:00"
+pos = "3:3"
---
+++ resson/r014.py
@@ -0,0 +1,9 @@
+#月曜日/三軒茶屋/相沢香Aさん/ユニバ(緊急事態宣言時)
+
+shop_id = "SHP051"
+string = "UNIVERSAL MUSIC WORLD DANCE"
+instructor = "相沢　香"
+#s_date = "2021-06-13"
+#e_date = "2021-06-13"
+time_s_reserve = "19:55:00"
+pos = "1:2"
