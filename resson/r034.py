a2a4faaaa823e5e55b389107f708531754685cca
kutu <kutsukake.kyi@gmail.com>
2021-07-08 08:12:39 +0900

    [水曜日] SAEさん追加

---
+++ resson/r034.py
@@ -0,0 +1,9 @@
+#水曜日/三軒茶屋/SAEさん/ユニバ
+
+shop_id = "SHP051"
+string = "UNIVERSAL MUSIC WORLD DANCE"
+instructor = "SAE"
+#s_date = "2021-06-13"
+#e_date = "2021-06-13"
+time_s_reserve = "21:10:00"
+pos = "1:5"
