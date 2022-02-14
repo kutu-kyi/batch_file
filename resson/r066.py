997f713fdff86ec0492958dfdf98c204318aeaa4
kutu <kutsukake.kyi@gmail.com>
2022-02-14 09:20:32 +0900

    [スケジュール] 更新

---
+++ resson/r066.py
@@ -0,0 +1,9 @@
+#土曜日/三軒茶屋/三木田さん/ユニバ
+
+shop_id = "SHP051"
+string = "UNIVERSAL MUSIC WORLD DANCE"
+instructor = "三木田　和香奈"
+#s_date = "2021-06-13"
+#e_date = "2021-06-13"
+time_s_reserve = "12:25:00"
+pos = "2:4"
--- resson/r082.py
+++ resson/r082.py
@@ -1,9 +1,9 @@
-#祝日スケジュール/宮崎台/高木先生/ジャズダンス
+#祝日スケジュール/喜多見/彩さん/ZONE
 
-shop_id = "SHP026"
-string = "ジャズダンス45"
-instructor = "高木　伸一郎"
+shop_id = "SHP034"
+string = "【CARDIOトレーニング】ZONE"
+instructor = "高橋　彩"
 #s_date = "2021-06-13"
 #e_date = "2021-06-13"
-time_s_reserve = "10:55:00"
+time_s_reserve = "13:05:00"
 pos = "1:4"
