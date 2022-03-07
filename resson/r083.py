badc609890a08d0b80846213c9e1f0467f3c78bf
kutu <kutsukake.kyi@gmail.com>
2022-03-07 11:32:09 +0900

    [develop] 3/21祝日小川先生

--- resson/r083.py
+++ resson/r083.py
@@ -1,9 +1,9 @@
-#祝日スケジュール/宮崎台/木村さん/フルステップ
+#祝日スケジュール/宮崎台/小川先生/ユニバ
 
 shop_id = "SHP026"
-string = "FULLSTEP MOVEMENT"
-instructor = "木村　亜莉沙"
+string = "UNIVERSAL MUSIC WORLD DANCE"
+instructor = "小川　ゆり"
 #s_date = "2021-06-13"
 #e_date = "2021-06-13"
-time_s_reserve = "12:25:00"
-pos = "1:4"
+time_s_reserve = "15:50:00"
+pos = "1:6"
