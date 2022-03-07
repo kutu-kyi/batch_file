f43bd96291dac6dde4477ad1f7e513b8ef941207
kutu <kutsukake.kyi@gmail.com>
2022-03-07 11:37:32 +0900

    [084] 3/21祝日スケジュール

--- resson/r084.py
+++ resson/r084.py
@@ -1,9 +1,9 @@
-#祝日スケジュール/三軒茶屋/ASAMIさん/BIG DANCE
+#祝日スケジュール/宮崎台/星野さん/JAZZ Dance
 
-shop_id = "SHP051"
-string = "BIG DANCE"
+shop_id = "SHP026"
+string = "ジャズダンス45"
 instructor = "Ａｓａｍｉ"
 #s_date = "2021-06-13"
 #e_date = "2021-06-13"
-time_s_reserve = "10:55:00"
+time_s_reserve = "14:50:00"
 pos = "1:5"
