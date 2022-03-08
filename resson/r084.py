6d90babba3f569b2d4af83395e865af911f29caf
kutu <kutsukake.kyi@gmail.com>
2022-03-08 16:47:37 +0900

    [2022/03/21] 祝日スケジュール

--- resson/r084.py
+++ resson/r084.py
@@ -1,9 +1,9 @@
-#祝日スケジュール/宮崎台/星野さん/JAZZ Dance
+#祝日スケジュール/新百合ヶ丘/山本あゆ美さん/ユニバ
 
-shop_id = "SHP026"
-string = "ジャズダンス45"
-instructor = "Ａｓａｍｉ"
+shop_id = "SHP035"
+string = "UNIVERSAL MUSIC WORLD DANCE60"
+instructor = "山本　あゆ美"
 #s_date = "2021-06-13"
 #e_date = "2021-06-13"
-time_s_reserve = "14:50:00"
+time_s_reserve = "16:55:00"
 pos = "1:5"
